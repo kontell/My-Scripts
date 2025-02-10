828f02c4b9be21863704508570bc5835c8a8bfdc
kontell <91025994+kontell@users.noreply.github.com>
2025-02-10 16:36:49 +0000

    Update grab-epg.sh

--- grab-epg.sh
+++ grab-epg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define variables
-EPG_GRABBER_PATH="/home/kontell/epg"
-GUIDE_PATH="/media/guides"
+EPG_GRABBER_PATH="/home/user/epg"
+GUIDE_PATH="/media//guides"
 CUTOFF_TIME=$(date -u -d '1 day ago' +%Y%m%d%H%M%S) # Generate timestamp for guide trim (UTC)
 
 # Set number of days from argument (default to 9 if not provided)
@@ -51,19 +51,16 @@ tv_sort \
    ${GUIDE_PATH}/tmp/sky.com.xml \
    --output ${GUIDE_PATH}/tmp/sky.com.sorted.xml \
    &
-
 tv_sort \
    --by-channel \
    ${GUIDE_PATH}/tmp/player.ee.co.uk.xml \
    --output ${GUIDE_PATH}/tmp/player.ee.co.uk.sorted.xml \
    &
-
 tv_sort \
    --by-channel \
    ${GUIDE_PATH}/tmp/dstv.com.xml \
    --output ${GUIDE_PATH}/tmp/dstv.com.sorted.xml \
    &
-
 tv_sort \
    --by-channel \
    ${GUIDE_PATH}/tmp/sky.co.nz.xml \
@@ -76,28 +73,27 @@ tv_merge \
    -i ${GUIDE_PATH}/guide.xml \
    -m ${GUIDE_PATH}/tmp/sky.com.sorted.xml \
    -o ${GUIDE_PATH}/guide.xml
-
 tv_merge \
    -i ${GUIDE_PATH}/guide.xml \
    -m ${GUIDE_PATH}/tmp/player.ee.co.uk.sorted.xml \
    -o ${GUIDE_PATH}/guide.xml
-
 tv_merge \
    -i ${GUIDE_PATH}/guide.xml \
    -m ${GUIDE_PATH}/tmp/dstv.com.sorted.xml \
    -o ${GUIDE_PATH}/guide.xml
-
 tv_merge \
    -i ${GUIDE_PATH}/guide.xml \
    -m ${GUIDE_PATH}/tmp/sky.co.nz.sorted.xml \
    -o ${GUIDE_PATH}/guide.xml
 
+# Reordered <display-name> to be the first child inside <channel>
+sed -i -E '/<channel id=/ {N;N; s#(<channel id="[^"]+">\n)\s*<url>(.*?)</url>\n\s*<display-name>(.*?)</display-name>#\1  <display-name>\3</display-name>\n  <url>\2</url>#}' ${GUIDE_PATH}/guide.xml
+
+# Fix indentaiton
+xmllint --format ${GUIDE_PATH}/guide.xml -o ${GUIDE_PATH}/guide.xml
 
 # Trim Old Programs
 tv_grep \
    --on-after "$CUTOFF_TIME" \
    ${GUIDE_PATH}/guide.xml \
    --output ${GUIDE_PATH}/guide.xml
-
-
-
